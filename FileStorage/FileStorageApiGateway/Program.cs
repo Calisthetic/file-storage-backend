eef6ddce67160a90781e507223047c913ee08daa
Calisthetic <vovakalash04@mail.ru>
2023-10-08 21:33:35 +0500

    patch/options reqs fix

--- FileStorage/FileStorageApiGateway/Program.cs
+++ FileStorage/FileStorageApiGateway/Program.cs
@@ -31,6 +31,19 @@ builder.Services.AddEndpointsApiExplorer();
 // Swagger for ocelot
 builder.Services.AddSwaggerGen();
 
+// CORS
+var MyAllowSpecificOrigins = "MyPolicy";
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+        policy =>
+        {
+            policy.WithOrigins("http://localhost:3000", "https://file-storage-frontend.vercel.app").AllowAnyHeader().AllowAnyMethod();
+        });
+});
+
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -43,6 +56,9 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
+
+// Added
+app.UseCors(MyAllowSpecificOrigins);
 
 app.UseSwaggerForOcelotUI(options =>
 {
